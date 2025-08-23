13b8ca814147b410fe7a3ba37cb83e5b6922798f
Shravankumar Janawade <shravanjanwade2252@gmail.com>
2025-08-23 04:24:43 +0000

    work on next.config editor feature and related files

---
+++ web-ide/next.config.ts
@@ -0,0 +1,24 @@
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
